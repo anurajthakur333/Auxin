7bd1068745a5692d880090c1766e412c33c27613
anurajthakur333 <anurajthakur141@gmail.com>
2025-04-26 12:20:50 +0530

    navbar added transparency

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,20 @@
+import "../styles/main.css";
+import "../styles/Navbar.css";
+
+
 const Navbar = () => {
 
   return (
   <nav className="navbar navbar-expand-lg py-1">
       <div className="container">
           <div className="navbar-nav mx-auto align-items-center gap-5">
-            <a className="nav-link text-white" href="#experience">Experience</a>
-            <a className="nav-link text-white" href="#articles">Articles</a>
+            <a className="nav-link text-white auxin-font " href="#experience">Experience</a>
+            <a className="nav-link text-white auxin-font " href="#articles">Articles</a>
             <div className="mx-4">
               <img src="/auxin.svg" alt="Auxin Logo" width="72" height="36" />
             </div>
-            <a className="nav-link text-white" href="#about">About Us</a>
-            <a className="nav-link text-white" href="#appointments">Appointments</a>
+            <a className="nav-link text-white auxin-font " href="#about">About Us</a>
+            <a className="nav-link text-white auxin-font " href="#appointments">Appointments</a>
         </div>
       </div>
     </nav>
