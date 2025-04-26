50369b96a2f994cb97a808e6138fd53338ace32d
anurajthakur333 <anurajthakur141@gmail.com>
2025-04-26 12:28:28 +0530

    code cleaned up

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,13 +8,13 @@ const Navbar = () => {
   <nav className="navbar navbar-expand-lg py-1">
       <div className="container">
           <div className="navbar-nav mx-auto align-items-center gap-5">
-            <a className="nav-link text-white auxin-font " href="#experience">Experience</a>
-            <a className="nav-link text-white auxin-font " href="#articles">Articles</a>
+            <a className="nav-link text-white auxin-font" href="#experience">Experience</a>
+            <a className="nav-link text-white auxin-font" href="#articles">Articles</a>
             <div className="mx-4">
               <img src="/auxin.svg" alt="Auxin Logo" width="72" height="36" />
             </div>
-            <a className="nav-link text-white auxin-font " href="#about">About Us</a>
-            <a className="nav-link text-white auxin-font " href="#appointments">Appointments</a>
+            <a className="nav-link text-white auxin-font" href="#about">About Us</a>
+            <a className="nav-link text-white auxin-font" href="#appointments">Appointments</a>
         </div>
       </div>
     </nav>
