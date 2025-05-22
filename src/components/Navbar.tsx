36097356aeb1286acb7c06f6d5186dd4b051494b
anurajthakur333 <anurajthakur141@gmail.com>
2025-05-22 18:22:39 +0530

    navbar font size fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,13 +8,13 @@ const Navbar = () => {
   <nav className="navbar navbar-expand-lg py-1 fixed-bottom">
       <div className="container">
           <div className="navbar-nav mx-auto align-items-center gap-5">
-            <a className="nav-link text-white aeonik-regular" href="#experience">Experience</a>
-            <a className="nav-link text-white aeonik-regular" href="#articles">Articles</a>
+            <a className="nav-link text-white aeonik-light" href="#experience">Experience</a>
+            <a className="nav-link text-white aeonik-light" href="#articles">Articles</a>
             <div className="mx-4">
               <img src="/auxin.svg" alt="Auxin Logo" width="72" height="36" />
             </div>
-            <a className="nav-link text-white aeonik-regular" href="#about">About Us</a>
-            <a className="nav-link text-white aeonik-regular" href="#appointments">Appointments</a>
+            <a className="nav-link text-white aeonik-light" href="#about">About Us</a>
+            <a className="nav-link text-white aeonik-light" href="#appointments">Appointments</a>
         </div>
       </div>
     </nav>
