08e3f554a0297721a11b13e317ebd2eeff173a8c
Auxin Media <gurjittsinggh@gmail.com>
2025-05-24 20:01:15 +0530

    Navbar v1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,14 +5,14 @@ import "../styles/Navbar.css";
 const Navbar = () => {
 
   return (
-  <nav className="navbar navbar-expand-lg fixed-bottom" style={{ height: '60px' }}>
+  <nav className="navbar navbar-expand-lg fixed-bottom" style={{ height: '50px' }}>
       <div className="container-fluid justify-content-center">
-          <div className="navbar-nav gap-5">
-            <a className="nav-link text-white aeonik-light" href="#experience">Experience</a>
-            <a className="nav-link text-white aeonik-light" href="#articles">Articles</a>
-              <img src="/auxin.svg" alt="Auxin Logo" width="85" height="41.9" />
-            <a className="nav-link text-white aeonik-light" href="#about">About Us</a>
-            <a className="nav-link text-white aeonik-light" href="#appointments">Appointments</a>
+          <div className="navbar-nav gap-5 d-flex align-items-center">
+            <a className="nav-link text-white aeonik-light px-3 fw-light" href="#experience">Experience</a>
+            <a className="nav-link text-white aeonik-light px-3 fw-light" href="#articles">Articles</a>
+              <img src="/auxin.svg" alt="Auxin Logo" width="85" height="35" />
+            <a className="nav-link text-white aeonik-light px-3 fw-light" href="#about">About Us</a>
+            <a className="nav-link text-white aeonik-light px-3 fw-light" href="#appointments">Appointments</a>
         </div>
       </div>
     </nav>
