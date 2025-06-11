67417ace006a7349e211b25664faa308e357d595
anurajthakur333 <anurajthakur141@gmail.com>
2025-06-11 23:58:06 +0530

    navlinks gap added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   }}
 >
   {/* Left side links */}
-  <div className="d-flex justify-content-end gap-4">
+  <div className="d-flex justify-content-end" style={{ gap: '2rem' }}>
     <a href="#experience" className="nav-link text-white aeonik-light">
       EXPERIENCE
     </a>
@@ -27,10 +27,10 @@ const Navbar = () => {
   </div>
 
   {/* Center logo */}
-  <img src="/auxin.svg" alt="Auxin Logo" width="85" height="35" />
+  <img src="/auxin.svg" alt="Auxin Logo" width="85" height="35" style={{ margin: '0 2rem' }} />
 
   {/* Right side links */}
-  <div className="d-flex justify-content-start gap-4">
+  <div className="d-flex justify-content-start" style={{ gap: '2rem' }}>
     <a href="#about" className="nav-link text-white aeonik-light">
       ABOUT US
     </a>
@@ -39,7 +39,6 @@ const Navbar = () => {
     </a>
   </div>
 </div>
-
   )
 }
 
