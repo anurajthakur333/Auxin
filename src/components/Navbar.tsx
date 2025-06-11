6703d37b562e0d5aa525f2922c73e82f0b130bce
anurajthakur333 <anurajthakur141@gmail.com>
2025-06-11 21:53:46 +0530

    commented nav links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,127 +1,130 @@
 import "../styles/fonts.css";
 import "../styles/Navbar.css";
-import ScrambleText from "./Scramble";
+// import ScrambleText from "./Scramble";
 
 const Navbar = () => {
-
   return (
-  <nav className="navbar navbar-expand-lg fixed-bottom" style={{ height: '50px' }}>
+    <nav className="navbar navbar-expand-lg fixed-bottom" style={{ height: '50px' }}>
       <div className="container-fluid d-flex justify-content-center">
-          <div className="d-flex align-items-center gap-5">
-            <a 
-              className="nav-link aeonik-light px-4 fw-light navbar-item" 
-              href="#experience" 
+        <div className="d-flex align-items-center gap-5">
+          {/* Navigation links commented out
+          <a 
+            className="nav-link aeonik-light px-4 fw-light navbar-item" 
+            href="#experience" 
+            style={{ 
+              textDecoration: 'none',
+              display: 'inline-block',
+              position: 'relative'
+            }}
+          >
+            <ScrambleText
+              trigger="hover"
+              speed="fast"
+              revealSpeed={0.9}
+              scrambleIntensity={5}
+              delay={0}
               style={{ 
-                textDecoration: 'none',
-                display: 'inline-block',
-                position: 'relative'
+                color: "white",
+                transition: "color 0.3s ease",
+                display: "inline-block",
+                whiteSpace: "nowrap"
               }}
+              scrambleColor="#39FF14"
+              glowEffect={false}
+              waveEffect={false}
             >
-              <ScrambleText
-                trigger="hover"
-                speed="fast"
-                revealSpeed={0.9}
-                scrambleIntensity={5}
-                delay={0}
-                style={{ 
-                  color: "white",
-                  transition: "color 0.3s ease",
-                  display: "inline-block",
-                  whiteSpace: "nowrap"
-                }}
-                scrambleColor="#39FF14"
-                glowEffect={false}
-                waveEffect={false}
-              >
-                EXPERIENCE
-              </ScrambleText>
-            </a>
-            <a 
-              className="nav-link aeonik-light px-3 ms-4 fw-light navbar-item" 
-              href="#articles" 
+              EXPERIENCE
+            </ScrambleText>
+          </a>
+          <a 
+            className="nav-link aeonik-light px-3 ms-4 fw-light navbar-item" 
+            href="#articles" 
+            style={{ 
+              textDecoration: 'none',
+              display: 'inline-block',
+              position: 'relative'
+            }}
+          >
+            <ScrambleText
+              trigger="hover"
+              speed="fast"
+              revealSpeed={0.9}
+              scrambleIntensity={5}
+              delay={0}
               style={{ 
-                textDecoration: 'none',
-                display: 'inline-block',
-                position: 'relative'
+                color: "white",
+                transition: "color 0.3s ease",
+                display: "inline-block",
+                whiteSpace: "nowrap",
+                paddingLeft: "10px"
               }}
+              scrambleColor="#39FF14"
+              glowEffect={false}
+              waveEffect={false}
             >
-              <ScrambleText
-                trigger="hover"
-                speed="fast"
-                revealSpeed={0.9}
-                scrambleIntensity={5}
-                delay={0}
-                style={{ 
-                  color: "white",
-                  transition: "color 0.3s ease",
-                  display: "inline-block",
-                  whiteSpace: "nowrap",
-                  paddingLeft: "10px"
-                }}
-                scrambleColor="#39FF14"
-                glowEffect={false}
-                waveEffect={false}
-              >
-                ARTICLES
-              </ScrambleText>
-            </a>
-              <img src="/auxin.svg" alt="Auxin Logo" width="85" height="35" />
-            <a 
-              className="nav-link aeonik-light pe-4 fw-light navbar-item" 
-              href="#about" 
+              ARTICLES
+            </ScrambleText>
+          </a>
+          */}
+          <img src="/auxin.svg" alt="Auxin Logo" width="85" height="35" />
+          {/* Navigation links commented out
+          <a 
+            className="nav-link aeonik-light pe-4 fw-light navbar-item" 
+            href="#about" 
+            style={{ 
+              textDecoration: 'none',
+              display: 'inline-block',
+              position: 'relative'
+            }}
+          >
+            <ScrambleText
+              trigger="hover"
+              speed="fast"
+              revealSpeed={0.9}
+              scrambleIntensity={5}
+              delay={0}
               style={{ 
-                textDecoration: 'none',
-                display: 'inline-block',
-                position: 'relative'
+                color: "white",
+                transition: "color 0.3s ease",
+                display: "inline-block",
+                whiteSpace: "nowrap"
               }}
+              scrambleColor="#39FF14"
+              glowEffect={false}
+              waveEffect={false}
             >
-              <ScrambleText
-                trigger="hover"
-                speed="fast"
-                revealSpeed={0.9}
-                scrambleIntensity={5}
-                delay={0}
-                style={{ 
-                  color: "white",
-                  transition: "color 0.3s ease",
-                  display: "inline-block",
-                  whiteSpace: "nowrap"
-                }}
-                scrambleColor="#39FF14"
-                glowEffect={false}
-                waveEffect={false}
-              >
-                ABOUT US
-              </ScrambleText>
-            </a>
-            <a 
-              className="nav-link aeonik-light px-4 fw-light navbar-item" 
-              href="#appointments" 
+              ABOUT US
+            </ScrambleText>
+          </a>
+          <a 
+            className="nav-link aeonik-light px-4 fw-light navbar-item" 
+            href="#appointments" 
+            style={{ 
+              textDecoration: 'none',
+              display: 'inline-block',
+              position: 'relative'
+            }}
+          >
+            <ScrambleText
+              trigger="hover"
+              speed="fast"
+              revealSpeed={0.9}
+              scrambleIntensity={5}
+              delay={0}
               style={{ 
-                textDecoration: 'none',
-                display: 'inline-block',
-                position: 'relative'
+                color: "white",
+                transition: "color 0.3s ease",
+                display: "inline-block",
+                whiteSpace: "nowrap"
               }}
+              scrambleColor="#39FF14"
+              glowEffect={false}
+              waveEffect={false}
             >
-              <ScrambleText
-                trigger="hover"
-                speed="fast"
-                revealSpeed={0.9}
-                scrambleIntensity={5}
-                delay={0}
-                style={{ 
-                  color: "white",
-                  transition: "color 0.3s ease",
-                  display: "inline-block",
-                  whiteSpace: "nowrap"
-                }}
-                scrambleColor="#39FF14"
-                glowEffect={false}
-                waveEffect={false}
-              >
-                APPOINTMENTS
-              </ScrambleText>
-            </a>
+              APPOINTMENTS
+            </ScrambleText>
+          </a>
+          */}
         </div>
       </div>
     </nav>
