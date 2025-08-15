75bfa5b7ed84c00ee1f4d01b0f9c440698459112
anurajthakur333 <anurajthakur141@gmail.com>
2025-08-16 02:04:16 +0530

    deploy error fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.ttf', '**/*.otf'], // This ensures TTF and OTF files are handled as assets
+  assetsInclude: ['**/*.ttf', '**/*.otf', '**/*.ico'], // Also treat .ico files as static assets
   build: {
     assetsDir: 'assets',
     rollupOptions: {
